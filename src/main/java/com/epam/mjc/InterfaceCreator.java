8ba5b0b484dbcb1c84b094b887fa228e78e739bc
Yerke Nurgali <70740293+qelephant@users.noreply.github.com>
2023-12-11 16:36:51 +0600

    Update InterfaceCreator.java

--- src/main/java/com/epam/mjc/InterfaceCreator.java
+++ src/main/java/com/epam/mjc/InterfaceCreator.java
@@ -1,5 +1,4 @@
-package com.epam.mjc;
-
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.function.BiFunction;
@@ -7,26 +6,33 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 public class InterfaceCreator {
 
-    public Predicate<List<String>> isValuesStartWithUpperCase() {
-        throw new UnsupportedOperationException("You should implement this method.");
-    }
-
-    public Consumer<List<Integer>> addEvenValuesAtTheEnd() {
-        throw new UnsupportedOperationException("You should implement this method.");
-    }
-
-    public Supplier<List<String>> filterCollection(List<String> values) {
-        throw new UnsupportedOperationException("You should implement this method.");
-    }
-
     public Function<List<String>, Map<String, Integer>> stringSize() {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return list -> list.stream().collect(Collectors.toMap(s -> s, String::length));
+        
     }
 
     public BiFunction<List<Integer>, List<Integer>, List<Integer>> concatList() {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return (list1, list2) -> {
+            List<Integer> result = new ArrayList<>(list1);
+            result.addAll(list2);
+            return result;
+        };
     }
+
+
+    public Predicate<List<String>> isValuesStartWithUpperCase() {
+        return list -> list.stream().allMatch(s -> s.matches("[A-Z].*"));
+    }
+
+
+    public Consumer<List<Integer>> addEvenValuesAtTheEnd() {
+        return list -> {
+            list.addAll(list.stream().filter(i -> i % 2 == 0).toList());
+        };
+    }
+    
 }
